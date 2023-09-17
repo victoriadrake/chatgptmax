a812f1b2cd2d6580c095eff436eefcf42f9c6101
Victoria Drake <24644237+victoriadrake@users.noreply.github.com>
2023-09-17 15:22:01 -0500

    Add automated testing

--- chatgptmax.py
+++ chatgptmax.py
@@ -9,52 +9,70 @@ import re
 # Load your API key from an environment variable or secret management service
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+
 def clean(text):
     """
     Cleans the provided text by removing URLs, email addresses, non-letter characters, and extra whitespace.
-    
+
     Args:
     - text (str): The input text to be cleaned.
-    
+
     Returns:
     - str: The cleaned text.
     """
     # Remove URLs
-    text = re.sub(r'http\S+', '', text)
-    
+    text = re.sub(r"http\S+", "", text)
+
     # Remove email addresses
-    text = re.sub(r'\S+@\S+', '', text)
-    
+    text = re.sub(r"\S+@\S+", "", text)
+
     # Remove everything that's not a letter (a-z, A-Z)
-    text = re.sub(r'[^a-zA-Z\s]', '', text)
-    
+    text = re.sub(r"[^a-zA-Z\s]", "", text)
+
     # Remove whitespace, tabs, and new lines
-    text = ''.join(text.split())
+    text = "".join(text.split())
 
     return text
+
 
 def clean_stopwords(text: str) -> str:
     """
     Removes common stopwords from the provided text.
-    
+
     Args:
     - text (str): The input text from which stopwords should be removed.
-    
+
     Returns:
     - str: The text with stopwords removed.
     """
-    stopwords = ["a", "an", "and", "at", "but", "how", "in", "is", "on", "or", "the", "to", "what", "will"]
+    stopwords = [
+        "a",
+        "an",
+        "and",
+        "at",
+        "but",
+        "how",
+        "in",
+        "is",
+        "on",
+        "or",
+        "the",
+        "to",
+        "what",
+        "will",
+    ]
     tokens = text.split()
     clean_tokens = [t for t in tokens if not t in stopwords]
     return " ".join(clean_tokens)
 
+
 def readData(file):
     """
     Reads the content of a file and returns it as a string.
-    
+
     Args:
     - file (str): The path to the file to be read.
-    
+
     Returns:
     - str: The content of the file.
     """
@@ -63,13 +81,14 @@ def readData(file):
         text = f.read()
     return text
 
+
 def clean_text_from_file(file):
     """
     Reads the content of a file, cleans it by removing stopwords, and returns the cleaned text.
-    
+
     Args:
     - file (str): The path to the file whose content should be cleaned.
-    
+
     Returns:
     - str: The cleaned content of the file or an error message if the file could not be read.
     """
@@ -80,7 +99,13 @@ def clean_text_from_file(file):
     return clean_stopwords(text)
 
 
-def send(prompt=None, text_data=None, chat_model="gpt-3.5-turbo", model_token_limit=8192, max_tokens=2500):
+def send(
+    prompt=None,
+    text_data=None,
+    chat_model="gpt-3.5-turbo",
+    model_token_limit=8192,
+    max_tokens=2500,
+):
     """
     Send the prompt at the start of the conversation and then send chunks of text_data to ChatGPT via the OpenAI API.
     If the text_data is too long, it splits it into chunks and sends each chunk separately.
@@ -99,45 +124,50 @@ def send(prompt=None, text_data=None, chat_model="gpt-3.5-turbo", model_token_li
         return "Error: Prompt is missing. Please provide a prompt."
     if not text_data:
         return "Error: Text data is missing. Please provide some text data."
-    
+
     # Initialize the tokenizer
     tokenizer = tiktoken.encoding_for_model(chat_model)
 
     # Encode the text_data into token integers
     token_integers = tokenizer.encode(text_data)
-    
+
     # Split the token integers into chunks based on max_tokens
     chunk_size = max_tokens - len(tokenizer.encode(prompt))
-    chunks = [token_integers[i:i + chunk_size] for i in range(0, len(token_integers), chunk_size)]
-    
+    chunks = [
+        token_integers[i : i + chunk_size]
+        for i in range(0, len(token_integers), chunk_size)
+    ]
+
     # Decode token chunks back to strings
     chunks = [tokenizer.decode(chunk) for chunk in chunks]
-    
+
     responses = []
-    messages = [{"role": "user", "content": prompt},
-                {"role": "user", "content": "To provide the context for the above prompt, I will send you text in parts. When I am finished, I will tell you 'ALL PARTS SENT'. Do not answer until you have received all the parts."}]
+    messages = [
+        {"role": "user", "content": prompt},
+        {
+            "role": "user",
+            "content": "To provide the context for the above prompt, I will send you text in parts. When I am finished, I will tell you 'ALL PARTS SENT'. Do not answer until you have received all the parts.",
+        },
+    ]
 
     for chunk in chunks:
         messages.append({"role": "user", "content": chunk})
-        
+
         # Check if total tokens exceed the model's limit and remove oldest chunks if necessary
-        while sum(len(tokenizer.encode(msg["content"])) for msg in messages) > model_token_limit:
+        while (
+            sum(len(tokenizer.encode(msg["content"])) for msg in messages)
+            > model_token_limit
+        ):
             messages.pop(1)  # Remove the oldest chunk
-        
-        response = openai.ChatCompletion.create(
-        model=chat_model,
-        messages=messages
-        )
-        chatgpt_response = response.choices[0].message['content'].strip()
+
+        response = openai.ChatCompletion.create(model=chat_model, messages=messages)
+        chatgpt_response = response.choices[0].message["content"].strip()
         responses.append(chatgpt_response)
-    
+
     # Add the final "ALL PARTS SENT" message
     messages.append({"role": "user", "content": "ALL PARTS SENT"})
-    response = openai.ChatCompletion.create(
-        model=chat_model,
-        messages=messages
-    )
-    final_response = response.choices[0].message['content'].strip()
+    response = openai.ChatCompletion.create(model=chat_model, messages=messages)
+    final_response = response.choices[0].message["content"].strip()
     responses.append(final_response)
-    
+
     return responses
---
+++ test_chatgptmax.py
@@ -0,0 +1,52 @@
+import pytest
+from chatgptmax import clean, clean_stopwords, readData, clean_text_from_file, send
+
+# Test for the clean function
+def test_clean():
+    text = "Check out this link: http://example.com and email me at test@example.com!"
+    cleaned_text = clean(text)
+    assert cleaned_text == "Checkoutthislinkandemailmeat"
+
+
+# Test for the clean_stopwords function
+def test_clean_stopwords():
+    text = "This is a test to remove stopwords from the text."
+    cleaned_text = clean_stopwords(text)
+    assert cleaned_text == "This test remove stopwords from text."
+
+
+# Test for the readData function
+def test_readData(tmp_path):
+    file = tmp_path / "test.txt"
+    file.write_text("This is a test file.")
+    assert readData(file) == "This is a test file."
+
+
+# Test for the clean_text_from_file function
+def test_clean_text_from_file(tmp_path):
+    file = tmp_path / "test.txt"
+    file.write_text("This is a test to remove stopwords from the file.")
+    cleaned_text = clean_text_from_file(file)
+    assert cleaned_text == "This test remove stopwords from file."
+
+
+# Test for the send function
+def test_send():
+    prompt = "Summarize the following text for me:"
+    text_data = "This is a test text to be summarized by ChatGPT."
+    response = send(prompt, text_data)
+    assert isinstance(response, list)
+    assert len(response) > 0
+
+
+# Test for the send function with missing prompt
+def test_send_missing_prompt():
+    response = send()
+    assert response == "Error: Prompt is missing. Please provide a prompt."
+
+
+# Test for the send function with missing data
+def test_send_missing_data():
+    prompt = "Summarize the following text for me:"
+    response = send(prompt)
+    assert response == "Error: Text data is missing. Please provide some text data."
